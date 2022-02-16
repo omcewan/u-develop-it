3551cd33e78e15dff7c3661af38dcc27edb03e99
omcewan <orlandomcewan231@gmail.com>
2022-02-16 05:42:13 -0500

    added queries for the mysql server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
     // Your MySQL password
     password: "decadent@bestow8inquire",
     database: "election",
-    port: "3001"
+    // port: "3001"
   },
   console.log("Connected to the election database.")
 );
@@ -26,11 +26,38 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
   console.log(rows);
 });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

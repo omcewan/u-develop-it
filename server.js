d7e25f7995e67d81ac2e0d83c079db9932d31cf9
omcewan <orlandomcewan231@gmail.com>
2022-02-15 21:28:49 -0500

    set up connection to mysql database anc how to query data from database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mysql = require("mysql2");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,10 +8,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "decadent@bestow8inquire",
+    database: "election",
+    port: "3001"
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
